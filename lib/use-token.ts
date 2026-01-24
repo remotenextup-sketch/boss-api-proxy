9a799942c43107e0fe16684140f0a9245a1dd1ce
remotenextup-sketch <remote.nextup@gmail.com>
2026-01-24 12:49:22 +0900

    Create use-token.ts

---
+++ lib/use-token.ts
@@ -0,0 +1,16 @@
+// lib/use-token.ts
+import { kv } from '@upstash/kv';
+
+export async function setTokens(tokens: { accessToken: string; refreshToken: string }) {
+  await kv.set('BOSS_ACCESS_TOKEN', tokens.accessToken);
+  await kv.set('BOSS_REFRESH_TOKEN', tokens.refreshToken);
+}
+
+export async function getTokensFromKV() {
+  const accessToken = await kv.get('BOSS_ACCESS_TOKEN');
+  const refreshToken = await kv.get('BOSS_REFRESH_TOKEN');
+  if (!accessToken || !refreshToken) throw new Error('access token not found in KV');
+  return { accessToken, refreshToken };
+}
+
+
